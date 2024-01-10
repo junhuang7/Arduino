9afbc48babd3f2edca3fe4240e7962560007e60d
Jun Huang <44811884+junhuang7@users.noreply.github.com>
2024-01-10 13:40:20 +0100

    LED_BUILTIN for test

--- trigger_led/trigger_led.ino
+++ trigger_led/trigger_led.ino
@@ -1,8 +1,8 @@
 void setup() {
-  pinMode(7, OUTPUT);   // Set the digital pin 7 as output
-  digitalWrite(7, HIGH); // Send a high signal
+  pinMode(LED_BUILTIN, OUTPUT);   // Set the digital pin 7 as output
+  digitalWrite(LED_BUILTIN, HIGH); // Send a high signal
   delay(1000);            // Wait for 1000 milliseconds
-  digitalWrite(7, LOW);  // Send a low signal
+  digitalWrite(LED_BUILTIN, LOW);  // Send a low signal
 }
 
 void loop() {
